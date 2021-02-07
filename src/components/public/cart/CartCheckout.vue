ea2542afe53e6abb9b22cfe0956c5a1e049ae2d5
Eduardo Silva <eduardo@dudstecnologia.com.br>
2021-02-07 19:44:03 -0400

    Loading ao retornar transação

--- src/components/public/cart/CartCheckout.vue
+++ src/components/public/cart/CartCheckout.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="mb-3">
-    <div v-if="productsCart.length > 0">
-      <h5 class="mb-3">The total amount of</h5>
+    <b-overlay :show="overlayStatus" rounded="sm">
+      <div v-if="productsCart.length > 0">
+        <h5 class="mb-3">The total amount of</h5>
 
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-          <div>
-            <strong>Total</strong>
-          </div>
-          <span><strong>$ {{ cartValue }}</strong></span>
-        </li>
-      </ul>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+            <div>
+              <strong>Total</strong>
+            </div>
+            <span><strong>$ {{ cartValue }}</strong></span>
+          </li>
+        </ul>
 
-      <div id="paypal-button-container"></div>
-    </div>
+        <div id="paypal-button-container"></div>
+      </div>
+    </b-overlay>
   </div>
 </template>
 
@@ -46,34 +48,33 @@ export default {
                 });
             },
             onApprove: (data, actions) => {
+              this.overlayStatus = true
+
               return this.$http.get('/purchase-capture/' + data.orderID)
                 .then((res) => {
-                    return res.data;
+                  return res.data
                 }).then((orderData) => {
-                    // Three cases to handle:
-                    //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                    //   (2) Other non-recoverable errors -> Show a failure message
-                    //   (3) Successful transaction -> Show confirmation or thank you
+                  this.overlayStatus = false
 
-                    var errorDetail = Array.isArray(orderData.details) && orderData.details[0]
+                  var errorDetail = Array.isArray(orderData.details) && orderData.details[0]
 
-                    if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
-                        return actions.restart()
-                    }
+                  if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
+                    return actions.restart()
+                  }
 
-                    if (errorDetail) {
-                        var msg = 'Sorry, your transaction could not be processed.'
-                        if (errorDetail.description) msg += '\n\n' + errorDetail.description
-                        if (orderData.debug_id) msg += ' (' + orderData.debug_id + ')'
+                  if (errorDetail) {
+                    var msg = 'Sorry, your transaction could not be processed.'
+                    if (errorDetail.description) msg += '\n\n' + errorDetail.description
+                    if (orderData.debug_id) msg += ' (' + orderData.debug_id + ')'
 
-                        this.$swal.fire('Ops!', msg, 'error')
+                    this.$swal.fire('Ops!', msg, 'error')
 
-                        return null
-                    }
+                    return null
+                  }
 
-                    this.clearCart()
-                    this.$swal.fire('Perfect', 'Transaction completed, thank you', 'success')
-                    this.$router.replace('/')
+                  this.clearCart()
+                  this.$swal.fire('Perfect', 'Transaction completed, thank you', 'success')
+                  this.$router.replace('/')
                 });
             }
           }).render('#paypal-button-container')
@@ -101,3 +102,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.b-overlay {
+  z-index: 1000 !important;
+}
+</style>
